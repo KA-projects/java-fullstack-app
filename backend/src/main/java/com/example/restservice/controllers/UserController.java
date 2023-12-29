0dec0ac6e235f0e44d7524e95fe9267ea9ca3ca6
KA-projects <122209044+KA-projects@users.noreply.github.com>
2023-12-29 17:42:48 +0600

    postgresql successfully tested using Postman

---
+++ backend/src/main/java/com/example/restservice/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.example.restservice.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.restservice.model.User;
+import com.example.restservice.services.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.creatUser(user);
+    }
+
+    @GetMapping
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return userService.updateUser(id, userDetails);
+    }
+}
---
+++ backend/src/main/java/com/example/restservice/services/UserService.java
@@ -0,0 +1,39 @@
+package com.example.restservice.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.restservice.model.User;
+import com.example.restservice.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User creatUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User existingUser = user.get();
+            existingUser.setName(userDetails.getName());
+            existingUser.setEmail(userDetails.getEmail());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+}
